ead121986b2b43b277de6ee271e3663625f3f39c
Sebastiaan Visser <sfvisser@cs.uu.nl>
2009-08-19 18:21:27 +0200

    Exposed Network.Salvia module.

--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@ clear
 ./Setup.lhs configure &&
 ./Setup.lhs build &&
 sudo ./Setup.lhs install --global 
-sudo ./dist/build/salvia-demo/salvia-demo +RTS -N2
+# sudo ./dist/build/salvia-demo/salvia-demo +RTS -N2
