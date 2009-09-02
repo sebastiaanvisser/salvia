96a639e00e295b5f0b4c924beda35a54ca5895d8
Sebastiaan Visser <sfvisser@cs.uu.nl>
2009-09-02 16:06:12 +0100

    Working client code.

--- src/Network/Salvia/Core/Client.hs
+++ src/Network/Salvia/Core/Client.hs
@@ -1,45 +1,52 @@
 module Network.Salvia.Core.Client where
 
 import Control.Applicative
-import Control.Monad.State
+import Control.Monad.State hiding (get)
+import Data.Maybe
 import Data.Record.Label
 import Network.BSD
 import Network.Protocol.Uri
 import Network.Salvia.Core.Aspects
 import Network.Salvia.Core.Handler
-import Network.Salvia.Handler.Client
 import Network.Salvia.Handler.Body
+import Network.Salvia.Handler.Client
 import Network.Socket
 import System.IO
 import qualified Network.Salvia.Core.Context as C
 
-client :: Handler c () a -> Handler c1 () b -> String -> IO b
+client
+  :: Handler c () a -- ^ Handler to setup request.
+  -> Handler d () b -- ^ Handler to react to response.
+  -> String         -- ^ jaja
+  -> IO (Maybe b)  -- ^ jaja
+
 client hReq hRes uri = 
-  do let u = toURI uri
-     entry <- getHostByName (lget host u)
-     sck <- socket (hostFamily entry) Stream 0
-     let p = let p' = lget port u in if p' == (-1) then 80 else p'
-     let addr = head (hostAddresses entry)
-     connect sck (SockAddrInet (fromIntegral p) addr)
-     h <- socketToHandle sck ReadWriteMode
-     name <- getSocketName sck
+  case parseUri uri of
+    Left _ -> return Nothing
+    Right u ->
+      do let p = fromMaybe 80 (get port u)
 
-     let ctx = C.mkContext (error "no server config") () name sck h
-     ss <- execStateT (unHandler hReq) ctx
+         -- Open up connection to client
+         entry <- getHostByName (get host u)
+         sck <- socket (hostFamily entry) Stream 0
+         let addr = head (hostAddresses entry)
+         connect sck (SockAddrInet (fromIntegral p) addr)
+         h <- socketToHandle sck ReadWriteMode
+         name <- getSocketName sck
 
-     let ctx' = C.mkContext (error "no server config") () name sck h
-     let k = request (put (lget C.request ss)) >> hRes
-     evalStateT (unHandler k) ctx'
+         -- Create context and run setup request.
+         ss <- execStateT (unHandler hReq) (C.mkContext undefined () name sck h)
 
-
-
+         -- Update context and handle response.
+         let k = request (put (get C.request ss)) >> hRes
+         Just <$> evalStateT (unHandler k) (C.mkContext undefined () name sck h)
 
 getRequest :: String -> IO (Maybe String)
-getRequest u = join <$>
+getRequest u = join . join <$>
   client 
     (hGetRequest u)
     (hClientEnvironment
       (const (return Nothing))
-      (asASCII hResponseContents)
+      (hResponseBody "utf-8")
     ) u
 
--- src/Network/Salvia/Handler/Client.hs
+++ src/Network/Salvia/Handler/Client.hs
@@ -1,43 +1,28 @@
 module Network.Salvia.Handler.Client where
 
-import Control.Monad.State
+import Control.Monad.State hiding (get)
 import Data.Record.Label
 import Network.Protocol.Http
 import Network.Protocol.Uri
 import Network.Salvia.Core.Aspects
-import Network.Salvia.Handler.Body
 import Network.Salvia.Handler.Parser
 import Network.Salvia.Handler.Printer
-import System.IO
+import qualified Control.Monad.State as S
 
-hGetRequest :: (HttpM Request m, SendM m) => String -> m ()
+hGetRequest :: (HttpM Request m, FlushM Request m) => String -> m ()
 hGetRequest s =
-  do let u = toURI s
+  do let u = toUri s
      request $
        do method     =: GET
-          uri        =: (lget path u ++ "?" ++ lget query u)
-          hostname   =: Just (show $ lget authority u)
+          uri        =: (get path u ++ "?" ++ get query u)
+          hostname   =: Just (show (get authority u))
           userAgent  =: Just "salvia-client"
           connection =: Just "close"
      hRequestPrinter
      return ()
 
 hClientEnvironment
-  :: (SocketM m, HttpM Response m, MonadIO m) =>
-     (String -> m a) -> m a -> m (Maybe a)
+  :: (SocketM m, HttpM Response m, MonadIO m)
+  => (String -> m a) -> m a -> m (Maybe a)
 hClientEnvironment = hResponseParser (4 * 1000)
-
-
-cHandler :: (HttpM Request m, SocketM m, MonadIO m, HttpM Response m) => m ()
-cHandler =
-  do q <- request get
-     liftIO (print q)
-     r <- response get
-     liftIO (print r)
-     c <- asASCII hResponseContents
-     liftIO (putStr ((\(Just s) -> s) c))
-     return ()
-
-
-
 
