43e2c518aa868188924b1f859a6b1a5e5f0a1e3c
Sebastiaan Visser <sfvisser@cs.uu.nl>
2009-10-17 12:19:03 +0200

    Added some documentation to the aspects.

--- src/Network/Salvia/Core/Aspects.hs
+++ src/Network/Salvia/Core/Aspects.hs
@@ -9,17 +9,30 @@ import Network.Socket
 import System.IO
 import qualified Data.ByteString.Lazy as B
 
+-- | The `HttpM' type class indicates is parametrized with the directon
+-- (`Request' or `Response') for which the implementation should be able to
+-- supply and modify the values. The `http` method allow for running arbitrary
+-- state computations over the request or response objects.
+
+class (Applicative m, Monad m) => HttpM dir m where
+  http :: State (Http dir) a -> m a
+
+-- | Stub request and response used to fill in type level gaps for message
+-- directions.
+
 forRequest :: Request
 forRequest = undefined
 
 forResponse :: Response
 forResponse = undefined
 
-class (Applicative m, Monad m) => HttpM d m where
-  http :: State (Http d) a -> m a
+-- | Type class alias indicating an HttpM instance for both requests and
+-- responses.
 
 class (HttpM Request m, HttpM Response m) => HttpM' m
 instance (HttpM Request m, HttpM Response m) => HttpM' m
+
+-- | Direction specific aliases for the `http' method.
 
 request :: HttpM Request m => State (Http Request) a -> m a
 request = http
@@ -27,36 +40,58 @@ request = http
 response :: HttpM Response m => State (Http Response) a -> m a
 response = http
 
+-- | The `PeerM` type class allows access to peer (the other endpoint of the
+-- connection) specific information like the socket and asscociated file handle
+-- and the socket address.
+
 class (Applicative m, Monad m) => PeerM m where
   rawSock :: m Socket
   sock    :: m Handle
   peer    :: m SockAddr
 
--- TODO:  queue and dequeue are probably enough.
 type SendAction = (Socket, Handle) -> IO ()
+
+-- | The `QueueM' type class allows for queing actions for sending data values
+-- over the wire. Using a queue for collecting send actions instead of directly
+-- sending values over the socket allows for a more modular client or server
+-- layout.
 
 class (Applicative m, Monad m) => QueueM m where
   enqueue  :: SendAction -> m ()
   dequeue  :: m (Maybe SendAction)
 
+  -- TODO:  queue and dequeue are probably enough.
   sendStr  :: String                                   -> m ()
   sendBs   :: B.ByteString                             -> m ()
   spoolStr :: (String       -> String)       -> Handle -> m ()
   spoolBs  :: (B.ByteString -> B.ByteString) -> Handle -> m ()
 
-class (Applicative m, Monad m) => FlushM d m where
-  flushHeaders  :: d -> m ()
-  flushQueue    :: d -> m ()
+-- | The `FlushM' type class can be used to flush the message headers and the
+-- message body directly over the wire to the other endpoint.
 
-class (Applicative m, Monad m) => BodyM d m where
-  body :: d -> m (Maybe B.ByteString)
+class (Applicative m, Monad m) => FlushM dir m where
+  flushHeaders :: dir -> m ()
+  flushQueue   :: dir -> m ()
+
+class (Applicative m, Monad m) => BodyM dir m where
+  body :: dir -> m (Maybe B.ByteString)
+
+-- | The `ServerM' type class can be used to acesss the static server
+-- configuration.
 
 class (Applicative m, Monad m) => ServerM m where
   server :: m Config
 
-class (Applicative m, Monad m) => PayloadM m p where
-  payload :: State p a -> m a
+-- | The `ClientM' type class can be used to acesss the static client
+-- configuration. Unit for now.
 
 class (Applicative m, Monad m) => ClientM m where
   client :: m ()
+
+-- | The `PayloadM' type class provides access to the server payload. The
+-- payload can be an arbitrary piece of data that gets shared between all the
+-- handlers. Can be used to implement sessions.
+
+class (Applicative m, Monad m) => PayloadM m p where
+  payload :: State p a -> m a
 
