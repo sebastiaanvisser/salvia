8073503725c6d00bdcdd8e7b97653dd3d41590ed
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-06-29 16:37:54 +0200

    Export protocol modules from Salvia.

--- src/Network/Salvia.hs
+++ src/Network/Salvia.hs
@@ -1,10 +1,18 @@
 module Network.Salvia
-( module Network.Salvia.Interface
+( -- module Network.Protocol.Cookie
+  module Network.Protocol.Http
+, module Network.Protocol.Mime
+, module Network.Protocol.Uri
+, module Network.Salvia.Interface
 , module Network.Salvia.Handlers
 , module Network.Salvia.Impl
 )
 where
 
+-- import Network.Protocol.Cookie
+import Network.Protocol.Http hiding (hostname)
+import Network.Protocol.Mime
+import Network.Protocol.Uri hiding (host)
 import Network.Salvia.Interface
 import Network.Salvia.Handlers
 import Network.Salvia.Impl
