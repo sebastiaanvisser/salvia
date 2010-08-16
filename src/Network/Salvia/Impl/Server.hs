deae4b98d8cd342432e3429a46af1aeac47fb02d
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-08-16 17:50:04 +0200

    Minor cleanup server code.

--- src/Network/Salvia/Impl/Server.hs
+++ src/Network/Salvia/Impl/Server.hs
@@ -5,7 +5,6 @@ import Control.Concurrent.Thread (ThreadId, forkIO, wait_, threadId)
 import Control.Concurrent.MVar (newMVar, modifyMVar_, readMVar)
 import Control.Exception (finally, block, unblock)
 import Control.Monad.State
-import Data.Functor ((<$))
 import Network.Protocol.Http hiding (accept, hostname)
 import Network.Salvia.Impl.Config
 import Network.Salvia.Impl.Context
@@ -13,67 +12,78 @@ import Network.Salvia.Impl.Handler
 import Network.Socket
 import System.IO
 
-{- | todo:
+{- |
 Start a webserver with a specific server configuration and default handler. The
 server will go into an infinite loop and will repeatedly accept client
-connections on the address and port specified in the configuration. For every
-connection the specified handler will be executed with the client address and
+connections on the address/port combinations specified in the configuration.
+For every connection the handler will be executed with the client address and
 socket stored in the handler context.
 -}
 
-{- todo:
-Start a listening TCP server on the specified address/port combination and
-handle every connection with a custom handler. Accept connections on the
-listening socket and pass execution to the application specific connection
-handler.
--}
-
 start :: Config -> Handler p () -> p -> IO ()
-start conf handler payload = forM (listenOn conf) (forkIO . listener) >>= waitAll
-    where
-      listener (SockAddrInet6 _ _ _ _) = error $ funcName ++ ": IPv6 sockets are not supported!"
-      listener (SockAddrUnix _)        = error $ funcName ++ ": Unix sockets are not supported!"
-      listener (SockAddrInet port addr) = do
-        inet_ntoa addr >>= \a ->
-          putStrLn ("starting listening server on: " ++ a ++ ":" ++ show port)
-        s <- socket AF_INET Stream 0
-        setSocketOption s ReuseAddr 1
-        bindSocket s sAddr
-        listen s (backlog conf)
-        handlersTidsMVar <- newMVar []
-        forever (acceptHandle handlersTidsMVar s) `finally` (readMVar handlersTidsMVar >>= waitAll)
-        where
-          sAddr = SockAddrInet port addr
+start conf handler payload =
+  do listeners <- forM (listenOn conf) (forkIO . ipv4only startOne)
+     waitAll listeners
 
-          acceptHandle handlersTidsMVar s = do
-            a <- accept s
-            modifyMVar_ handlersTidsMVar $ \handlersTids ->
-                fmap (:handlersTids) $ block $ forkIO $
-                     unblock (handle a) `finally` deleteMyTid handlersTidsMVar
+  where
 
-          handle (sck, cAddr) = do
-            hndl <- socketToHandle sck ReadWriteMode
-            void $ runHandler handler Context
-                 { _cServerHost  = domain    conf
-                 , _cAdminMail   = adminMail conf
-                 , _cListenOn    = listenOn  conf
-                 , _cPayload     = payload
-                 , _cRequest     = emptyRequest
-                 , _cResponse    = emptyResponse
-                 , _cRawRequest  = emptyRequest
-                 , _cRawResponse = emptyResponse
-                 , _cSocket      = sck
-                 , _cHandleIn    = hndl
-                 , _cHandleOut   = hndl
-                 , _cClientAddr  = cAddr
-                 , _cServerAddr  = sAddr
-                 , _cQueue       = []
-                 }
+  startOne port addr =
+    do -- Print out on which addres this server is running.
+       a <- inet_ntoa addr
+       putStrLn ("starting listening server on: " ++ a ++ ":" ++ show port)
 
-          deleteMyTid handlersTidsMVar = do
-            myTid <- myThreadId
-            modifyMVar_ handlersTidsMVar $ \handlersTids ->
-                return $! deleteWhen' ((myTid ==) . threadId) handlersTids
+       -- Setup socket and start listening.
+       s <- socket AF_INET Stream 0
+       setSocketOption s ReuseAddr 1
+       let sAddr = SockAddrInet port addr
+       bindSocket s sAddr
+       listen s (backlog conf)
+
+       -- Setup variable to store thread IDs.
+       tids <- newMVar []
+
+       -- Go into loop to accept incoming connections.
+       let waitForAll = readMVar tids >>= waitAll
+       forever (acceptHandle sAddr tids s)
+         `finally` waitForAll
+
+       where
+
+         acceptHandle sAddr tids s = do
+           a <- accept s
+           modifyMVar_ tids $ \handlersTids ->
+               fmap (:handlersTids) $ block $ forkIO $
+                    unblock (handle sAddr a) `finally` deleteMyTid tids
+
+         handle sAddr (sck, cAddr) = do
+           hndl <- socketToHandle sck ReadWriteMode
+           _ <- runHandler handler Context
+                { _cServerHost  = domain    conf
+                , _cAdminMail   = adminMail conf
+                , _cListenOn    = listenOn  conf
+                , _cPayload     = payload
+                , _cRequest     = emptyRequest
+                , _cResponse    = emptyResponse
+                , _cRawRequest  = emptyRequest
+                , _cRawResponse = emptyResponse
+                , _cSocket      = sck
+                , _cHandleIn    = hndl
+                , _cHandleOut   = hndl
+                , _cClientAddr  = cAddr
+                , _cServerAddr  = sAddr
+                , _cQueue       = []
+                }
+           return ()
+
+         deleteMyTid tids = do
+           myTid <- myThreadId
+           modifyMVar_ tids $ \handlersTids ->
+               return $! deleteWhen' ((myTid ==) . threadId) handlersTids
+
+ipv4only :: (PortNumber -> HostAddress -> IO a) -> SockAddr -> IO a
+ipv4only _ (SockAddrInet6 _ _ _ _ ) = error (funcName ++ ": IPv6 sockets are not yet supported!")
+ipv4only _ (SockAddrUnix _        ) = error (funcName ++ ": Unix sockets are not yet supported!")
+ipv4only c (SockAddrInet port addr) = c port addr
 
 funcName :: String
 funcName = "Network.Salvia.Impl.Server.start"
@@ -97,5 +107,3 @@ deleteWhen' p = deleteWhen'_p
           | p x            = xs
           | otherwise      = (x:) $! deleteWhen'_p xs
 
-void :: Functor f => f a -> f ()
-void = (() <$)
